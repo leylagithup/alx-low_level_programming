31a58f9817d3789313de9be7261e48608a6f54b7
leyla makri <lili22leyla@gmail.com>
2023-04-21 11:33:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+char ch = 'a';
+while (ch <= '2')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n);
+return (0);
+}
